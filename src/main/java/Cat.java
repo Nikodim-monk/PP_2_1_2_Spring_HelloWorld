cd51d22e17b1128017066a8eaa6d864d5e11770a
Nikodim-monk <lr1975@yandex.ru>
2022-07-31 13:33:12 +0400

    проверка

---
+++ src/main/java/Cat.java
@@ -0,0 +1,13 @@
+public class Cat {
+    private String klichka;
+
+
+    public String getKlichka() {
+        return klichka;
+    }
+
+    public void setKlichka(String klichka) {
+        this.klichka = klichka;
+    }
+
+}
