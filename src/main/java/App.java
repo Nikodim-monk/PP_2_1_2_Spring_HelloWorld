75ac7e14b869dd9988161206aea61b7de0c4273d
Nikodim-monk <lr1975@yandex.ru>
2022-07-31 14:40:34 +0400

    проверка

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,16 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.hashCode()==bean2.hashCode());
+
+        Cat bean3 = applicationContext.getBean(Cat.class);
+        Cat bean4 = applicationContext.getBean(Cat.class);
+        System.out.println(bean3.hashCode()==bean4.hashCode());
     }
 }
+
+//                applicationContext.getBean("cat", Cat.class);
+//                (Cat) applicationContext.getBean("cat");
